da39fa4cf6705445f28d8914416719b987d0a181
Junior Alves <jrnalves@gmail.com>
2021-12-23 09:02:36 -0300

    create fetch API to gw2 api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-const fetchRaids = async () => await fetch('https://api.guildwars2.com/v2/account/raids?access_token=8CA26801-906E-8F4A-9FE0-05956985CAD305CB785A-AD42-4FE1-BBEC-746E3CC94256')
+const $form = document.querySelector('[data-js="form"]')
 
-const raids = async () => {
-    const raid = await (await fetchRaids()).json()
-    console.log(raid)
-}
+const fetchRaids = async APIKEY => await fetch(`https://api.guildwars2.com/v2/account/raids?access_token=${APIKEY}`)
 
-// raids()
+const getRaids = async APIKEY => await (await fetchRaids(APIKEY)).json()
+
+$form.addEventListener('submit', async event => {
+    event.preventDefault()
+    const APIKEY = event.target.inputAPI.value
+
+    const raidListDone = await getRaids('8CA26801-906E-8F4A-9FE0-05956985CAD305CB785A-AD42-4FE1-BBEC-746E3CC94256')
+
+    console.log(raidListDone)
+})
