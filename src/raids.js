32e6114e7b8b4981ded0a4c74b16e186d8992908
Junior Alves <jrnalves@gmail.com>
2021-12-31 10:39:34 -0300

    add POF raids

--- src/raids.js
+++ src/raids.js
@@ -7,6 +7,12 @@ const $raidWing3 = document.querySelector('[data-title="wing3"]')
 const $wing3 = document.querySelectorAll('[data-wing="wing3"]')
 const $raidWing4 = document.querySelector('[data-title="wing4"]')
 const $wing4 = document.querySelectorAll('[data-wing="wing4"]')
+const $raidWing5 = document.querySelector('[data-title="wing5"]')
+const $wing5 = document.querySelectorAll('[data-wing="wing5"]')
+const $raidWing6 = document.querySelector('[data-title="wing6"]')
+const $wing6 = document.querySelectorAll('[data-wing="wing6"]')
+const $raidWing7 = document.querySelector('[data-title="wing7"]')
+const $wing7 = document.querySelectorAll('[data-wing="wing7"]')
 
 const fetchRaids = async APIKEY => await fetch(`https://api.guildwars2.com/v2/account/raids?access_token=${APIKEY}`)
 
@@ -15,7 +21,6 @@ const getRaids = async APIKEY => await (await fetchRaids(APIKEY)).json()
 const converNodeToArray = wing => Array.prototype.slice.call(wing)
 
 const raidIsDone = (raidWing, wing) => {
-    console.log(wing)
     const nodeToArray = converNodeToArray(wing)
     const isDone = nodeToArray.every(element => element.classList.contains('fa-check'))
 
@@ -30,6 +35,9 @@ const raidDone = () => {
     raidIsDone($raidWing2, $wing2)
     raidIsDone($raidWing3, $wing3)
     raidIsDone($raidWing4, $wing4)
+    raidIsDone($raidWing5, $wing5)
+    raidIsDone($raidWing6, $wing6)
+    raidIsDone($raidWing7, $wing7)
 }
 
 const addCheckIcon = (deadBossList) => {
